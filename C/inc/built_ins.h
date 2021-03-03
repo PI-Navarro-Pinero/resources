abd12f575e9da65f6f2543f89430ce783c7cc795
Tomas <mrgreen@arquimedes.fibertel.com.ar>
2021-03-03 20:26:05 -0300

    puede ejecutar comandos mediante sockets. todo: enviar los resultados de la ejecucion al cliente

---
+++ C/inc/built_ins.h
@@ -0,0 +1,24 @@
+#ifndef BUILT_INS_H
+#define BUILT_INS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define STR_LEN       1024
+#define TOKEN_BUFSIZE 64
+#define TOKEN_DELIM " \t\r\n\a"
+
+void loop(char linea[STR_LEN]);//, int *estado);
+char **parsear(char *linea);
+int  run(char **args);
+int  ejecutar(char **args);
+
+int cd_com(char **args);
+int clr_com(char **args);
+int echo_com(char **args);
+int quit_com(char **args);
+
+#endif
---
+++ C/inc/sockets.h
@@ -0,0 +1,68 @@
+/**
+ * @file sockets.h
+ * @brief
+ * Header para el uso de los sockets.
+ *
+ * @author Tomás Santiago Piñero
+ */
+
+#include <stdint.h>
+
+#define IP_ADDR "127.0.0.1" /**< Dirección IP del servidor */
+#define PUERTO  7777        /**< Puerto para conectar con Java */
+#define STR_LEN 1024        /**< Largo de los strings */
+
+/**
+ * @brief
+ * Escribe en el socket deseado un mensaje.
+ * @param  sockfd Socket en el que se desea escribir el mensaje.
+ * @param  cmd    Comando enviado por el usuario.
+ * @return        Si el write fue exitoso.
+ */
+ssize_t send_cmd(int sockfd, void *cmd);
+
+/**
+ * @brief
+ * Lee lo que se encuentra en el socket.
+ * @param  sockfd Socket del que se desea leer el mensaje.
+ * @param  cmd    Comando enviado por el usuario.
+ * @return        Si el read fue exitoso.
+ */
+ssize_t recv_cmd(int sockfd, void *cmd);
+
+/**
+ * @brief
+ * Función encargada de crear el socket en el puerto pedido.
+ * @param  port Puerto al que se desea conectar.
+ s* @return      File descriptor del socket creado.
+ */
+int create_svsocket(char *ip, uint16_t port);
+
+/**
+ * @brief
+ * Función encargada de crear el socket y escuchar por posibles conexiones
+ * al mismo.
+ *
+ * @param  sockfd File descriptor del socket.
+ * @param  newfd  File descriptor del cliente.
+ * @return        File descriptor cliente-servidor.
+ */
+int start_listening(int sockfd, int newfd);
+
+/**
+ * @brief
+ * Loop en la que recibe los mensajes del cliente conectado.
+ * @param sockfd  File descriptor del servidor.
+ * @param newfd   File descriptor del cliente.
+ * @param msg_buf Buffer para almacenar los mensajes.
+ */
+void loop_listen(int sockfd, int newfd, char msg_buf[STR_LEN]);
+
+/**
+ * @brief
+ * Función encargada de checkear el valor de operaciones que devuelvan -1 cuando
+ * ocurre un error.
+ *
+ * @param err Número.
+ */
+void check_error(int err);
