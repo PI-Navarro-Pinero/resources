c6c456e7447d0f0bb64806675fd3c95ebceb6aaf
tomasspi <tom-300@hotmail.com>
2021-03-09 15:35:50 -0300

    la redireccion de stdin, stdout y stderr ahora es una fincion

--- C/inc/IO_redirection.h
+++ C/inc/IO_redirection.h
@@ -14,3 +14,4 @@ void input_redirection(char file_name[]);
 void output_redirection(char file_name[]);
 void borrar_simbolo(char **args, int index);
 int  check_redirection(char **args, char file_name[]);
+int  socket_reditection(int socket);
