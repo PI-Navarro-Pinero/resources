e36aec8ce4f9c689922280284c6e4bde228e05e7
tomasspi <tom-300@hotmail.com>
2021-03-14 11:22:10 -0300

    cambio indentacion

--- C/myshell.c
+++ C/myshell.c
@@ -14,29 +14,30 @@ int main(int argc, char* argv[])
   char buffer[STR_LEN];
   char **args;
 
-  newfd = start_listening(sockfd);//, newfd);
+  newfd = start_listening(sockfd);
 
   //Recibe los comandos del cliente, en caso de recibir un 'quit',
   //cierra la conexion y espera una conexion nueva
   while(1)
-  {
-    rw = recv_cmd(newfd, buffer);
-    check_error((int) rw);
-
-    if(!strcmp(buffer,"quit"))
-      {
-        newfd = start_listening(sockfd);
-        check_error(newfd);
-      }
-    else
     {
-      args = parsear(buffer);
-      rw = ejecutar(args, newfd);
+      rw = recv_cmd(newfd, buffer);
+      check_error((int) rw);
 
-      if(rw == 0)
-        send_cmd(newfd,"COMANDO NO SOPORTADO.\n");
+      if(!strcmp(buffer,"quit"))
+        {
+          send_cmd(newfd, "¡Adiós!\n");
+          newfd = start_listening(sockfd);
+          check_error(newfd);
+        }
+      else
+        {
+          args = parsear(buffer);
+          rw   = ejecutar(args, newfd);
+
+          if(rw == 0)
+            send_cmd(newfd,"Comando no soportado.\n");
+        }
     }
-  }
 
   return EXIT_SUCCESS;
 }
